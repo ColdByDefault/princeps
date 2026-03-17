4ac1ef5c1838b34377844e56e6803f9ab30be8fe
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-03-17 08:28:13 +0100

    feat: implement robots and sitemap generation; add German and English localization files

--- app/robots.ts
+++ app/robots.ts
@@ -2,3 +2,26 @@
  * @author ColdByDefault
  * @copyright 2026 ColdByDefault. All Rights Reserved.
  */
+
+import { type MetadataRoute } from "next";
+
+function getSiteUrl(): URL {
+  const baseUrl =
+    process.env.BETTER_AUTH_URL ??
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ??
+    "http://localhost:3000";
+
+  return new URL(baseUrl);
+}
+
+export default function robots(): MetadataRoute.Robots {
+  const siteUrl = getSiteUrl();
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: new URL("/sitemap.xml", siteUrl).toString(),
+  };
+}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,3 +3,23 @@
  * @copyright 2026 ColdByDefault. All Rights Reserved.
  */
 
+import { type MetadataRoute } from "next";
+
+function getSiteUrl(): URL {
+  const baseUrl =
+    process.env.BETTER_AUTH_URL ??
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ??
+    "http://localhost:3000";
+
+  return new URL(baseUrl);
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = getSiteUrl();
+  const lastModified = new Date();
+
+  return ["/", "/login", "/sign-up"].map((path) => ({
+    url: new URL(path, siteUrl).toString(),
+    lastModified,
+  }));
+}
