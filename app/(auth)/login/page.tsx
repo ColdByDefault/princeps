18e9b4ff022a923e412d3d4b724bbc37801f197d
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-03-18 17:43:57 +0100

    feat: refactor auth component imports to use named exports

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import LoginCard from "@/components/auth/LoginCard";
+import { LoginCard } from "@/components/auth";
 import { getRequestConfig } from "@/i18n/request";
 
 export default async function LoginPage() {
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@
  * @copyright  2026 ColdByDefault. All Rights Reserved.
  */
 
-import SignUpCard from "@/components/auth/SignUpCard";
+import { SignUpCard } from "@/components/auth";
 import { getRequestConfig } from "@/i18n/request";
 
 export default async function SignUpPage() {
