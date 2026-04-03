b0f9600a9c1ac282ca0a247dabc82319847fe479
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-04-03 15:30:17 +0200

    feat: enhance AppLayout with session management and improved structure

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,17 +3,23 @@
  * @copyright 2026 ColdByDefault. All Rights Reserved.
  */
 
-
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth/auth";
+import { Navbar, Footer } from "@/components/navigation";
 
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  const sessionUser = session?.user ?? null;
 
   return (
-    <div>
-      {children}
+    <div className="flex min-h-screen flex-col">
+      <Navbar sessionUser={sessionUser} />
+      <main className="flex flex-1 flex-col">{children}</main>
+      <Footer />
     </div>
   );
 }
