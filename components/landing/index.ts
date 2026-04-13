122157b2365248db270c5e226812613b183577ad
ColdByDefault <abo.ayash.yazan@gmail.com>
2026-04-13 17:32:08 +0200

    feat: refactor import path for Threads component and remove BackgroundThread export from landing index

--- components/landing/index.ts
+++ components/landing/index.ts
@@ -3,6 +3,5 @@
  * @copyright 2026 ColdByDefault. All Rights Reserved.
  */
 
-export { default as BackgroundThread } from "./BackgroundThread";
 export * from "./LandingHero";
 export { default as RotatingText } from "./LandingText";
